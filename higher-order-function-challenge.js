28093eb9e06dcd303914c753032766f7ca6c3cdd
johnmberger <johnmartinberger@gmail.com>
2016-07-13 23:17:58 -0600

    finished exercises

--- higher-order-function-challenge.js
+++ higher-order-function-challenge.js
@@ -1,11 +1,56 @@
-any = function(Array){
-  if (Array.pop() > 10){
-    return true;
-  } else {
-    return false;
+any([8,9,10,11], greaterThan10);
+// Returns true
+
+sumTwoNumbers(3)(9);
+// Returns 12
+
+filter([1, 2, 3, 4], onlyOdd);
+// // Returns [1, 3]
+
+surprise()();
+// Returns 'surprise!'
+
+// ----- Functions -----
+
+function any(arr, callback) {
+  for (var i = 0; i < arr.length; i++) {
+    if (callback(arr[i]) === true) {
+      return true;
+      break;
+    } else {
+      return false;
+    }
   }
 }
 
-greaterThan10 = function(){
+function greaterThan10(input) {
+  if (input > 10) {
+    return true;
+  }
+}
 
+function surprise() {
+  return function(){
+    return 'surprise!';
+  }
+}
+
+function onlyOdd(arr) {
+  var result = [];
+  for (i = 0; i<arr.length; i++) {
+    if (i % 2 !== 1){
+      result.push(arr[i]);
+    }
+  } return result;
+}
+
+function filter(arr, callback) {
+  var result = callback(arr);
+  return result;
+}
+
+function sumTwoNumbers(num) {
+  return function(input) {
+    return input + num;
+  }
 }
